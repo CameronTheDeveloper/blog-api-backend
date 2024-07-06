c089f389422f2804de97537709af1963d8ef3a62
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-06 03:16:46 -0700

    Add post field to BlogComment schema

--- models/blogComment.js
+++ models/blogComment.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const BlogCommentSchema = new Schema({
     text: { type: String, required: true, maxLength: 1000 },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
+    post: { type: Schema.Types.ObjectId, ref: 'BlogPost', required: true }
 }, { timestamps: true });
 
 BlogCommentSchema.virtual('url').get(function () {

594cd2d012a0e85b3b51d94d4bb0bd9eb896f5f7
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-06 05:14:46 -0700

    Add user routes with placeholder operations

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Root route for users
+router.get('/', function (req, res, next) {
+  res.send('GET users - not implemented');
+});
+
+
+// Routes for creating users
+router.get('/create', (req, res, next) => {
+  res.send('GET create user - not implemented');
+});
+
+router.post('/create', (req, res, next) => {
+  res.send('POST create user - not implemented');
+});
+
+// Routes for specific user operations
+router.get('/user/:userId', (req, res, next) => {
+  res.send('GET user by ID - not implemented');
+});
+
+router.put('/user/:userId', (req, res, next) => {
+  res.send('PUT user by ID - not implemented');
+});
+
+router.delete('/user/:userId', (req, res, next) => {
+  res.send('DELETE user by ID - not implemented');
 });
 
 module.exports = router;

af2a2da226be6b336a81049aa13417a4cf99e721
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-07 03:37:51 -0700

    Add route handlers for app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const routes = require('./routes');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -19,8 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogposts', routes.blogPosts);
+app.use('/comments', routes.blogComments);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const blogPosts = require('./blogPosts');
+const blogComments = require('./blogComments');
+const users = require('./users');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = {
+  blogPosts,
+  blogComments,
+  users
+};

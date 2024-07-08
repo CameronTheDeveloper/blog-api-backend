1644f2efbd99223a6e30133fb0837c43801a02c3
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-08 00:25:33 -0700

    Add user username & password minimum length

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true, maxLength: 40 },
-    password: { type: String, required: true, maxLength: 999 },
+    username: { type: String, required: true, minLength: 3, maxLength: 40 },
+    password: { type: String, required: true, minLength: 8, maxLength: 999 },
     isBlogAuthor: { type: Boolean, required: true },
 });
 

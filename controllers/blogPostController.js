ad29268b0b0c4e2a2a2e9ba4be8c12b541fd1206
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-15 01:02:35 -0700

    Add blogPost_post controller logic

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -10,3 +10,41 @@ exports.blogPost_list = asyncHandler(async (req, res, next) => {
         blogPost_list: blogPosts
     });
 });
+
+exports.blogPost_post = [
+    body('blogTitle')
+        .trim()
+        .escape()
+        .isLength({ min: 1 })
+        .withMessage('Title is required'),
+    body('blogText')
+        .trim()
+        .escape()
+        .isLength({ min: 1 })
+        .withMessage('Text is required'),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const blogPost = new BlogPost({
+            title: req.body.blogTitle,
+            text: req.body.blogText,
+            author: req.user._id,
+            isPublished: false,
+        });
+
+        if (!errors.isEmpty()) {
+            res.json({
+                message: 'Error filling out blog post form',
+                blogPost: blogPost,
+                errors: errors.array()
+            });
+        } else {
+            await blogPost.save();
+            res.json({
+                message: 'Created Blog Post',
+                blogPost: blogPost
+            });
+        }
+    })
+];

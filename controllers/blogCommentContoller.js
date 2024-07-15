07ee402515a4ee03dae35f41862749bee39741b7
Cameron Posey <cameronposey@outlook.com>
2024-07-15 15:30:10 -0700

    Add blog_comments_get controller logic

---
+++ controllers/blogCommentContoller.js
@@ -0,0 +1,12 @@
+const BlogComment = require('../models/blogComment');
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
+exports.blog_comments_get = asyncHandler(async(req, res, next) => {
+    const blogComments = await BlogComment.find(req.params.id).exec();
+
+    res.json({
+        message: 'Comments',
+        comment_list: blogComments
+    })
+});

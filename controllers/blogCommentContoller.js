d59bd9c708f57b281e752efa45d52d1f18de4948
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-15 19:35:40 -0700

    Fix blog_comments_get find() method

--- controllers/blogCommentContoller.js
+++ controllers/blogCommentContoller.js
@@ -2,11 +2,11 @@ const BlogComment = require('../models/blogComment');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
-exports.blog_comments_get = asyncHandler(async(req, res, next) => {
-    const blogComments = await BlogComment.find(req.params.id).exec();
+exports.blog_comments_get = asyncHandler(async (req, res, next) => {
+    const blogComments = await BlogComment.find({ post: req.params.id }).exec();
 
     res.json({
         message: 'Comments',
         comment_list: blogComments
-    })
+    });
 });

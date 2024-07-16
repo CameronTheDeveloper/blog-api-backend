7678561e61697e2fbee7dd3c5853bf783fb60ac0
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-16 03:43:21 -0700

    Add blog_comment_post controller logic

--- controllers/blogCommentContoller.js
+++ controllers/blogCommentContoller.js
@@ -10,3 +10,34 @@ exports.blog_comments_get = asyncHandler(async (req, res, next) => {
         comment_list: blogComments
     });
 });
+
+exports.blog_comment_post = [
+    body('commentText', 'Comment must have characters')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const comment = new BlogComment({
+            text: req.body.commentText,
+            author: req.body.author._id,
+            post: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            res.json({
+                message: 'Comment input error',
+                comment: comment,
+                errors: errors.array()
+            });
+        } else {
+            await comment.save();
+            res.json({
+                message: 'Comment posted',
+                comment: comment
+            });
+        }
+    })
+];
